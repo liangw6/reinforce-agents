b6dd22acb90adcde8472aa04e14f06f04fa52d47
Arthur Liang <liangw6@cs.washington.edu>
2021-03-03 19:33:37 -0800

    some modifications

--- hello_gym.py
+++ hello_gym.py
@@ -3,11 +3,13 @@ import gym
 
 env = gym.make("CartPole-v1")
 observation = env.reset()
-for _ in range(1000):
+for i in range(40):
   env.render()
   action = env.action_space.sample() # your agent here (this takes random actions)
   observation, reward, done, info = env.step(action)
 
   if done:
-    observation = env.reset()
+    # observation = env.reset()
+    print("Dead at", i)
+    break
 env.close()
