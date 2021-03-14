6a98819aad788d08fab98c9cc96753975610af16
Arthur Liang <liangw6@cs.washington.edu>
2021-03-13 17:02:29 -0800

    updates hello_gym with code reuse

--- hello_gym.py
+++ hello_gym.py
@@ -1,17 +1,21 @@
 import gym
 
 
-def cartPoleRun():
-    env = gym.make('CartPole-v0')
+def run_random_agent(env_name='CartPole-v0'):
+    env = gym.make(env_name)
     for i_episode in range(20):
         observation = env.reset()
         for t in range(100):
             env.render()
             print('at t', t)
-            print('\t observation:',observation)
+            print('\t observation:', observation)
             print('\t action space:', env.action_space)
 
+            # 4 actions: do nothing, left, center, right
             action = env.action_space.sample()
+            if action == 1 or action == 3:
+                # don't fire side engines
+                action = 0
             observation, reward, done, info = env.step(action)
 
             print('\t action', action)
@@ -22,20 +26,4 @@ def cartPoleRun():
     env.close()
 
 
-def lunarLander():
-    env = gym.make('LunarLander-v2')
-    for i_episode in range(20):
-        observation = env.reset()
-        for t in range(100):
-            env.render()
-            # print(observation)
-            action = env.action_space.sample()
-            observation, reward, done, info = env.step(action)
-            if done:
-                print("Episode finished after {} timesteps".format(t + 1))
-                break
-    env.close()
-
-
-# lunarLander()
-cartPoleRun()
+run_random_agent('LunarLander-v2')
